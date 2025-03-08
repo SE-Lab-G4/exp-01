9e87fbe66659e54134fd4fc79d19e3b322a80820
avacyrus10 <avacyrus0@gmail.com>
2025-03-08 17:34:10 +0330

    [feat] Add basic sensor data simulation script

---
+++ sensors/sensor.py
@@ -0,0 +1,14 @@
+import random
+import time
+
+def read_sensor_data():
+    return {
+        "temperature": round(random.uniform(20.0, 30.0), 2),
+        "humidity": round(random.uniform(30.0, 60.0), 2),
+        "timestamp": time.time()
+    }
+
+if __name__ == "__main__":
+    for _ in range(5):
+        print(read_sensor_data())
+        time.sleep(1)
